afc74e84ab74ffac324c214891bd6b21a6d60821
Solon P Pissis <solonas13@gmail.com>
2016-12-16 14:28:59 +0000

    bench

---
+++ examples/rmq-bench.cc
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include "rmq-offline.h"
+
+double gettime( void )
+{
+    struct timeval ttime;
+    gettimeofday( &ttime , 0 );
+    return ttime.tv_sec + ttime.tv_usec * 0.000001;
+};
+
+int main( int argc, char *argv[] )
+{
+    double start, end;
+    srand( time(NULL) );
+
+    INT n = atoi(argv[1]);
+    INT * A = ( INT * ) calloc ( n, sizeof(INT) );
+
+    for ( INT i= 0; i < n; i++ )
+   	A[i] = i;
+    for ( INT i = 0; i < (n/2); i++ )
+    {	
+   	INT a = rand() % n;
+   	INT b = rand() % n;
+        INT temp = A[a];
+        A[a] = A[b];
+        A[b] = temp;  
+    }
+
+    INT q = atoi(argv[2]);
+    Query * Q = ( Query * ) calloc ( q, sizeof( Query ) );
+    for ( INT i = 0; i < q; i++ )
+    {	
+   	INT a = rand() % n;
+   	INT b = rand() % n;
+   	if ( a <= b ) 
+        {
+           Q[i] . L = a; 
+           Q[i] . R = b;
+        }
+        else 
+        {
+           Q[i] . L = b; 
+           Q[i] . R = a;
+        }
+    }
+
+
+    fprintf( stderr, "n = %ld, q = %ld\n", n, q);
+    start = gettime();
+    rmq_offline ( A, n, Q, q );
+    end = gettime();
+    fprintf( stderr, "%lf\n", end - start);
+
+    free ( A );
+    free ( Q );
+
+    return ( 0 );
+}
