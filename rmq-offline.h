d13caee25443238e7a4c809474b329ac56766814
Solon P Pissis <solonas13@gmail.com>
2016-12-18 12:29:51 +0000

    comments

--- rmq-offline.h
+++ rmq-offline.h
@@ -22,9 +22,27 @@
 typedef int64_t INT;
 #endif
 
+/**
+ * Query struct.
+ *
+ * @param L The left index of the query
+ * @param R The right index of the query (R >= L)
+ * @param O The index of the minimum
+ */
+
 struct Query
 {
     INT L, R, O;
 };
+
+/**
+ * This is the only library function.
+ *
+ * @param A An array of type INT
+ * @param n The array size
+ * @param Q An array of type Query
+ * @param q The array size
+ * @return The answers are returned in Q
+ */
 
 INT rmq_offline ( INT * A, INT n, Query * Q, INT q );
