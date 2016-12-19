91fdf936866d1f3064fa8ae5e4f5730f9b68bb4b
Solon P Pissis <solonas13@gmail.com>
2016-12-19 20:48:04 +0000

    LCA example completed

--- examples/example-lca.cc
+++ examples/example-lca.cc
@@ -1,112 +1,76 @@
 #include <stdio.h>
 #include "rmq-offline.h"
 
-struct Node
-{
-    INT data;
-    Node * left, * right;
-};
-
-void TreeToArray ( Node * node, INT * A, INT * i );
-void deleteTree ( Node * node );
-Node* newNode( INT data);
-
 int main( int argc, char *argv[] )
 {
 	/*
-     		Let's construct a binary tree!
-            0
-          /  \
-         1    2
-       /  \
-      3    4
-
+     		Let's construct the Cartesian tree of sequence 4, 1, 2, 3, 0
+           0
+          /  
+         4    
+          \
+           1
+	    \
+             2
+              \
+               3	
 	*/
  
-    Node *root = newNode(0);
-    root->left        = newNode(1);
-    root->right       = newNode(2);
-    root->left->left  = newNode(3);
-    root->left->right = newNode(4);
+    // Some LCA Queries to answer: LCA(3,4), LCA(0,2), LCA(1,2), and LCA(2,3)
+    INT q = 4;
+    Query * Q_lca = ( Query * ) calloc ( q, sizeof( Query ) );
+    Q_lca[0] . L = 3; Q_lca[0] . R = 4;	
+    Q_lca[1] . L = 0; Q_lca[1] . R = 2;
+    Q_lca[2] . L = 1; Q_lca[2] . R = 2;
+    Q_lca[3] . L = 2; Q_lca[3] . R = 3;
 
-    // Some LCA Queries to answer: LCA(3,4), LCA(0,2), and LCA(1,2)
-    INT q = 3;
-    Query * Q = ( Query * ) calloc ( q, sizeof( Query ) );
-    Q[0] . L = 3; Q[0] . R = 4;	
-    Q[1] . L = 0; Q[1] . R = 2;
-    Q[2] . L = 1; Q[1] . R = 2;
-
-    // Transform the tree to an array via a DFS in-order traversal
+    // Write down the Euler tour, the depths array, and the representative array
     INT n = 5;
-    INT * A = ( INT* ) calloc( n , sizeof(INT) );
-    INT k = 0;
-    TreeToArray ( root, A, &k );
-    deleteTree ( root );
+    INT s = (2 * n)  - 1;
+    INT * E = ( INT * ) calloc( s , sizeof(INT) );
+    E[0] = 0; E[1] = 4; E[2] = 1; E[3] = 2; E[4] = 3; E[5] = 2; E[6] = 1; E[7] = 4; E[8] = 0;
+    INT * L = ( INT * ) calloc( s , sizeof(INT) );
+    L[0] = 0; L[1] = 1; L[2] = 2; L[3] = 3; L[4] = 4; L[5] = 3; L[6] = 2; L[7] = 1; L[8] = 0;
+    INT * R = ( INT * ) calloc( n , sizeof(INT) );
+    R[0] = 0; R[1] = 2; R[2] = 3; R[3] = 4; R[4] = 1;
 
-    // Record the indices of the node labels 0,1,...,4
-    INT * invA = ( INT* ) calloc( n , sizeof(INT) );
-    for ( INT i = 0; i < n; i ++ )  invA[A[i]] = i;
-
-    Query * Q_lca = ( Query * ) calloc( q , sizeof(Query) );
+    Query * Q = ( Query * ) calloc( q , sizeof( Query ) );
     for ( INT i = 0; i < q; i ++ )  
     {
-       if (invA[Q[i] . L] < invA[Q[i] . R])
+       if ( R[Q_lca[i] . L] < R[Q_lca[i] . R] )
        {
-	  Q_lca[i] . L = invA[Q[i] . L];
-	  Q_lca[i] . R = invA[Q[i] . R];
+	  Q[i] . L = R[Q_lca[i] . L];
+	  Q[i] . R = R[Q_lca[i] . R];
        }
        else
        {
-          Q_lca[i] . L = invA[Q[i] . R];
-	  Q_lca[i] . R = invA[Q[i] . L];
+          Q[i] . L = R[Q_lca[i] . R];
+	  Q[i] . R = R[Q_lca[i] . L];
        }
     }
-    free( invA );
  
     // Answer the queries!
-    rmq_offline ( A, n, Q_lca, q );
+    rmq_offline ( L, s, Q, q );
 
     // Transform the RMQ answers back to node labels
-    for ( INT i = 0; i < q; i++ )	Q[i] . O = A[Q_lca[i] . O];
+    for ( INT i = 0; i < q; i++ )	Q_lca[i] . O = E[Q[i] . O];
 
     // Printout the LCAs
     for ( INT i = 0; i < q; i++ )
     {	
       fprintf( stderr, "LCA(%ld,%ld)=%ld\n",
-                  Q[i].L,
-                  Q[i].R,
-                  Q[i].O);
+                  Q_lca[i].L,
+                  Q_lca[i].R,
+                  Q_lca[i].O);
     }		
 
     // Free the memory
     free ( Q );
     free( Q_lca );
-    free( A );
+    free( L );
+    free( E );
+    free( R );
 
 
     return ( 0 );
-}
-
-void deleteTree ( Node * node )
-{
-    if(node->left) deleteTree(node->left);
-    if(node->right) deleteTree(node->right);
-    free ( node );
-}
-
-/* Tree to Array */
-void TreeToArray ( Node * node, INT * A, INT * i )
-{
-    if (node == NULL) return;
-    TreeToArray (node->left, A, i );
-    A[*i] = node -> data; (*i)++;
-    TreeToArray (node->right, A, i );
-}
-
-Node* newNode( INT data)
-{
-    Node* node = ( Node * ) calloc ( 1, sizeof( Node ));
-    node->data = data;
-    node->left = node->right = NULL;
-    return(node);
 }
