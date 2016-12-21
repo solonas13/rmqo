3391d1f5e962fc321eb8a00b7e8e537c4f324350
Solon P Pissis <solonas13@gmail.com>
2016-12-21 17:26:03 +0000

    lca example

--- examples/example-lca.cc
+++ examples/example-lca.cc
@@ -4,19 +4,31 @@
 int main( int argc, char *argv[] )
 {
 	/*
-     		Let's consider the following binary tree:
-           0
-          /  
-         4    
-          \
-           1
-	    \
-             2
-              \
-               3	
+
+     	Let's consider the following binary tree:
+
+	           0
+	          /  
+	         4    
+	        / \
+	       5   1
+		    \
+       		     2
+         	      \
+                       3	
                
-               In fact we will consider its Euler tour representation...
+        In fact we will consider its Euler tour representation...
 	*/
+
+    // Write down the Euler tour array, the depths array, and the representative array
+    INT n = 6;
+    INT s = (2 * n)  - 1;
+    INT * E = ( INT * ) calloc( s , sizeof(INT) );
+    E[0] = 0; E[1] = 4; E[2] = 5; E[3] = 4; E[4] = 1; E[5] = 2; E[6] = 3; E[7] = 2; E[8] = 1; E[9] = 4; E[10] = 0;
+    INT * L = ( INT * ) calloc( s , sizeof(INT) );
+    L[0] = 0; L[1] = 1; L[2] = 2; L[3] = 1; L[4] = 2; L[5] = 3; L[6] = 4; L[7] = 3; L[8] = 2; L[9] = 1; L[10] = 0;
+    INT * R = ( INT * ) calloc( n , sizeof(INT) );
+    R[0] = 0; R[1] = 4; R[2] = 5; R[3] = 6; R[4] = 1;
  
     // Some LCA Queries to answer: LCA(3,4), LCA(0,2), LCA(1,2), and LCA(2,3)
     INT q = 4;
@@ -26,15 +38,6 @@ int main( int argc, char *argv[] )
     Q_lca[2] . L = 1; Q_lca[2] . R = 2;
     Q_lca[3] . L = 2; Q_lca[3] . R = 3;
 
-    // Write down the Euler tour array, the depths array, and the representative array
-    INT n = 5;
-    INT s = (2 * n)  - 1;
-    INT * E = ( INT * ) calloc( s , sizeof(INT) );
-    E[0] = 0; E[1] = 4; E[2] = 1; E[3] = 2; E[4] = 3; E[5] = 2; E[6] = 1; E[7] = 4; E[8] = 0;
-    INT * L = ( INT * ) calloc( s , sizeof(INT) );
-    L[0] = 0; L[1] = 1; L[2] = 2; L[3] = 3; L[4] = 4; L[5] = 3; L[6] = 2; L[7] = 1; L[8] = 0;
-    INT * R = ( INT * ) calloc( n , sizeof(INT) );
-    R[0] = 0; R[1] = 2; R[2] = 3; R[3] = 4; R[4] = 1;
 
     // Translate the LCA queries to RMQs
     Query * Q = ( Query * ) calloc( q , sizeof( Query ) );
